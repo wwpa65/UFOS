3f696a56bb34f8fab9b5b2b0c31b02cdeecb6eb6
Wimal Pathmasiri <wimal@Wimals-MBP.lan>
2022-05-06 13:34:58 -0400

    Updated JS Code

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    tbody.html("");
-  }
-
-function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
   
@@ -27,3 +23,29 @@ function buildTable(data) {
       );
     });
   }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
